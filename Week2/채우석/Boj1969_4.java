e0845e63ccc0c99060b26e6408bda9ab1243e46b
wochae <officialwochae@gmail.com>
2025-02-25 13:34:08 +0900

    [week2] : done

--- Week2/채우석/Boj1969_4.java
+++ Week2/채우석/Boj1969_4.java
@@ -4,8 +4,6 @@ package Week2.채우석;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 // DNA
@@ -13,19 +11,47 @@ public class Boj1969_4 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // static Map 을 가지고, 해당하는 위치를 반환해서 그 값에 1을 추가해주나?
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        Map<Integer, Character> map = new HashMap<>();
-        map.put(1, 'A');
-        map.put(2, 'T');
-        map.put(3, 'G');
-        map.put(4, 'C');
+
+        int[][] count = new int[4][K];
+        String[] dna = new String[N];
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            dna[i] = br.readLine();
             for (int j = 0; j < K; j++) {
-                // counting Char each position in string
+                char c = dna[i].charAt(j);
+                if (c == 'A') count[0][j]++;
+                else if (c == 'C') count[1][j]++;
+                else if (c == 'G') count[2][j]++;
+                else if (c == 'T') count[3][j]++;
             }
         }
+        StringBuilder resultData = new StringBuilder();
+        int hamSum = 0;
+
+        for (int j = 0; j < K; j++) {
+            int maxCount = 0;
+            char bestChar = 'A';
+
+            char[] bases = {'A', 'C', 'G', 'T'};
+            for (int i = 0; i < 4; i++) {
+                if (count[i][j] > maxCount) {
+                    maxCount = count[i][j];
+                    bestChar = bases[i];
+                }
+            }
+            resultData.append(bestChar);
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                if (dna[i].charAt(j) != resultData.charAt(j)) {
+                    hamSum++;
+                }
+            }
+        }
+        System.out.println(resultData.toString());
+        System.out.println(hamSum);
     }
 }
