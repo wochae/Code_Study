a8a38eed44c650d17ed6026f03a08acc65d931f9
하동헌 <149583958+imclaremont@users.noreply.github.com>
2025-03-03 17:14:24 +0900

    refactor: O(n^2) 에서 O(n) 으로

--- Week3/하동헌/4_동전 0.java
+++ Week3/하동헌/4_동전 0.java
@@ -17,9 +17,9 @@ public class Main {
 
         int cnt = 0;
         for (int i = N - 1; i >= 0; i--) {
-            while (arr[i] <= K) {
-                K -= arr[i];
-                cnt++;
+            if (arr[i] <= K) {
+                cnt += K / arr[i];
+                K %= arr[i];
             }
             if (K == 0) {
                 break;
